8b871f428ded2506df947494789a9e281bb6ad21
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-07 11:36:43 -0700

    adds a quit option

--- lib/machine.rb
+++ lib/machine.rb
@@ -8,32 +8,47 @@ class Machine
   def initialize
     file = File.read('./tiny_vending_machine.json')
     @contents = JSON.parse(file)
+    @money = Money.new
   end
 
   def intro(response = gets.chomp)
     if response == 'c'
       snacks = Snack.new(@contents)
       snacks.list
+      message
+      intro
     elsif response =='i'
       puts 'Please insert your money'
-      money = Money.new
-      money.total
+      @money.total
+      message
+      intro
     elsif response == 'v'
       puts 'Accepts coins of 1,5,10,25 Cents'
+      message
+      intro
     elsif response == 'p'
       puts "Please enter the slot of the item you'd like to vend"
+      message
+      intro
+    elsif response == 'q'
     else
-      puts 'please enter a valid response'
+      puts 'Please enter a valid response'
+      message
       intro
     end
   end
+
+  def message
+    puts 'Hello, please enter a command:'
+    puts '(c)ontents'
+    puts '(i)nsert money'
+    puts '(v)alid coin values'
+    puts '(p)urchase'
+    puts '(q)uit'
+  end
 end
 
-machine = Machine.new
-puts 'Hello, please enter a command:'
-puts '(c)ontents'
-puts '(i)nsert money'
-puts '(v)alid coin values'
-puts "(p)urchase"
 
+machine = Machine.new
+machine.message
 machine.intro
--- lib/money.rb
+++ lib/money.rb
@@ -1,13 +1,13 @@
+require './lib/machine'
 class Money
   attr_reader :contents
 
-  def initialize(data)
-    @contents = data['contents']
+  def initialize
     @total = 0
   end
 
   def total(response = gets.chomp)
-    if [1,5,10,25].includes? response.to_i
+    if [1,5,10,25].include? response.to_i
       @total += response.to_i
       puts "Total inserted: #{@total}"
     else
