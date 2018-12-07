bfb006105df3d9d69d6bc984459580ad8bbd12e0
Katelyn <katelyn.rimkunas@gmail.com>
2018-12-07 11:44:13 -0700

    string prompt needs line-breaks

--- objects/vm_interface.rb
+++ objects/vm_interface.rb
@@ -2,7 +2,7 @@
 
 class VMInterface
 
-  
+
 
 
   def start
@@ -14,10 +14,10 @@ class VMInterface
   private
 
   def greetings
-    "Hello, please enter a command:
-    (c)ontents - Prints the contents of the vending machine.
-    (i)nsert money - Takes in coins and prints total money inserted.
-    (v)alid coin values - Prints the valid coins inputs:  Accepts coins of 1,5,10,25 Cents i.e. penny, nickel, dime, and quarter.
+    "Hello, please enter a command:\n
+    (c)ontents - Prints the contents of the vending machine.\n
+    (i)nsert money - Takes in coins and prints total money inserted.\n
+    (v)alid coin values - Prints the valid coins inputs:  Accepts coins of 1,5,10,25 Cents i.e. penny, nickel, dime, and quarter.\n
     (p)urchase - Displays a prompt to enter the slot they'd like to purchase."
   end
 
