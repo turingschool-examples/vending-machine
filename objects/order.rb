73b514d4edb395e9f29485a3efbdf22adfd9be6b
Kate <36523304+Kate-v2@users.noreply.github.com>
2018-12-07 11:58:20 -0700

    Update order.rb

--- objects/order.rb
+++ objects/order.rb
@@ -6,7 +6,7 @@ class Order
     # position
     # sale_price
     # item
-    # needs to recieve the item from position
+    # needs to receive the item from position
     # NEED TO REWORK position to give item, and not just consume item in the message
     # should order send the message?? -- yes...?
     # NEED TO REWORK POSITION
