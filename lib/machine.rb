474dd66759a3e93d42fdefc647bea62beccba732
Jenn Lao <jenn.p.lao@gmail.com>
2018-12-07 10:31:26 -0700

    adds intro phrases

--- lib/machine.rb
+++ lib/machine.rb
@@ -6,4 +6,24 @@ class Machine
     file = File.read('./tiny_vending_machine.json')
     @contents = JSON.parse(file)
   end
+
+  def intro(response = gets.chomp)
+    if response == 'c'
+    elsif response =='i'
+    elsif response == 'v'
+    elsif response == 'p'
+    else
+      puts 'please enter a valid response'
+      intro
+    end
+  end
 end
+
+machine = Machine.new
+puts 'Hello, please enter a command:'
+puts '(c)ontents - Prints the contents of the vending machine.'
+puts '(i)nsert money - Takes in coins and prints total money inserted.'
+puts '(v)alid coin values - Prints the valid coins inputs:  Accepts coins of 1,5,10,25 Cents i.e. penny, nickel, dime, and quarter.'
+puts "(p)urchase - Displays a prompt to enter the slot they'd like to purchase."
+
+machine.intro
--- test/machine_test.rb
+++ test/machine_test.rb
@@ -4,10 +4,8 @@ require './lib/machine'
 require 'pry'
 
 class MachineTest < Minitest::Test
-  def test_it_can_get_a_section
+  def test_it_can_enter_responses
   machine = Machine.new
 
-  assert_equal "U.S.", story.section(1)
-  assert_equal "Technology", story.section(7)
-end
+  end
 end
